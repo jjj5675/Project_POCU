667d0a8aca46c93d1f0e70f580d8678aaf20880f
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-09-22 22:56:18 +0900

    Add Sample Code

---
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class BigNumberCalculator
+    {
+        public BigNumberCalculator(int bitCount, EMode mode)
+        {
+        }
+
+        public static string GetOnesComplementOrNull(string num)
+        {
+            string binaryIdentifier = num.Substring(0, 2);
+
+            if (!binaryIdentifier.Equals("0b"))
+            {
+                return null;
+            }
+
+            char[] binaryNumber = num.ToCharArray(2, num.Length - 2);
+            string output = binaryIdentifier;
+
+            foreach(var c in binaryNumber)
+            {
+                if(c == '0')
+                {
+                    output += "1";
+                }
+                else
+                {
+                    output += "0";
+                }
+            }
+
+            return output;
+        }
+
+        public static string GetTwosComplementOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToBinaryOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToHexOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToDecimalOrNull(string num)
+        {
+            return null;
+        }
+
+        public string AddOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+
+        public string SubtractOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+    }
+}
---
+++ Assignment1/Assignment1/EMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public enum EMode
+    {
+        Decimal, Binary
+    }
+}
