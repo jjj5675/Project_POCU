abfce37ac70b5878c390664f723b6e0b82d23338
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-11-04 19:30:33 +0900

    실습7 EFeatureFlags 열거형 구현

--- Lab7/Lab7/EFeatureFlags.cs
+++ Lab7/Lab7/EFeatureFlags.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Lab7
 {
-    class EFeatureFlags
+    [Flags]
+    public enum EFeatureFlags
     {
+        Default = 0,
+        Men = 1,
+        Women = 2,
+        Rectangle = 4,
+        Round = 8,
+        Aviator = 16,
+        Red = 32,
+        Blue = 64,
+        Black = 128
     }
 }
