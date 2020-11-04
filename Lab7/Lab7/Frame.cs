4e9a2d565614e2d953eab97cd5400aa610f70b14
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-11-04 19:38:19 +0900

    실습7 Frame 클래스 준비

--- Lab7/Lab7/Frame.cs
+++ Lab7/Lab7/Frame.cs
@@ -6,5 +6,30 @@ namespace Lab7
 {
     class Frame
     {
+        public EFeatureFlags Features { get; private set; }
+        public uint ID { get; private set; }
+        public string Name { get; private set; }
+
+
+        public Frame(uint id, string name)
+        {
+            ID = id;
+            Name = name;
+            Features = EFeatureFlags.Default;
+        }
+
+        public void ToggleFeatures(EFeatureFlags features)
+        {
+        }
+
+        public void TurnOnFeatures(EFeatureFlags features)
+        {
+
+        }
+
+        public void TurnOffFeatures(EFeatureFlags features)
+        {
+
+        }
     }
 }
