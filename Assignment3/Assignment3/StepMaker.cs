c7448c5db66813620f39eb3484e7f6fd2b4b4fd6
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-11-16 00:31:10 +0900

    과제3 준비

---
+++ Assignment3/Assignment3/StepMaker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment3
+{
+    public static class StepMaker
+    {
+        public static List<int> MakeSteps(int[] steps, INoise noise)
+        {
+            return null;
+        }
+    }
+}
